c3c47fb988765f30a1c76d84fc415449f4c98248
bharat_thapa <admin@bharatt.com.np>
2021-08-29 10:35:56 +0545

    uninstall feature added

--- uninstall.php
+++ uninstall.php
@@ -6,3 +6,12 @@
 if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN ') ) {
 	die( 'why though?' );
 }
+
+global $wpdb;
+$wpbtsc_options = 'submitcontent_options';
+$table_name = $wpdb->prefix . 'submitcontent';
+
+// delete plugin options
+delete_option( $wpbtsc_options );
+// drop plugin custom database table
+$wpdb->query( "DROP TABLE IF EXISTS {$table_name}" );
